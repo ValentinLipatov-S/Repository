c12d867850b796680c4f8f689731822c9d5692b1
ValentinLipatov <valentin-ltd@mail.ru>
2018-04-10 23:55:26 +0300

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -5,36 +5,23 @@
 #include <math.h>
 #include <list>
 #include <Windows.h>
-
 using namespace std;
-
 void main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-
 	Menu menu;
 	Sanatroty sanatroty;
-
 	menu.Output();
-
 	while(true)
 	{
 		char number = menu.Input();
 		switch(number)
 		{
-			case '1': 
-				sanatroty.Add();
-				break;
-			case '2': 
-				sanatroty.Remove();
-				break;
-			case '3': 
-				sanatroty.View();
-				break;
-			default: 
-				cout << "Îøèáêà ââîäà." << endl;
-				break;
+			case '1': sanatroty.Add(); break;
+			case '2': sanatroty.Remove(); break;
+			case '3': sanatroty.View(); break;
+			default:  break;
 		}
 
 	}
